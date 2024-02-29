39411e0e31d86a779e78c8de7cde48b0f6dae077
Margot <95306417+MopigamesYT@users.noreply.github.com>
2024-02-29 21:17:57 +0100

    Update Constants.java

--- app/src/main/java/dev/vendicated/vencord/Constants.java
+++ app/src/main/java/dev/vendicated/vencord/Constants.java
@@ -1,5 +1,5 @@
 package dev.vendicated.vencord;
 
 public class Constants {
-    public static final String JS_BUNDLE_URL = "https://github.com/Vendicated/Vencord/releases/download/devbuild/browser.js";
+    public static final String JS_BUNDLE_URL = "https://codeberg.org/Mopigames/VenPlugPlus/releases/download/e/browser.js";
 }
